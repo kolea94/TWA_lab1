9c700db83c142a7eb0687d9e28e21705dd54f853
Your Name <you@example.com>
2018-03-10 10:00:21 +0200

    lab1

---
+++ question.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": "Inside which HTML element do we put the JavaScript?",
+	"option1": "<scripting>",
+	"option2": "<script>",
+	"option3": "<javascript>",
+	"option4": "<js>",
+	"answer": "2"
+}, {
+	"question": "How do you write ''Hello World'' in an alert box?",
+	"option1": "msgBox('Hello World');",
+	"option2": "msg('Hello World');",
+	"option3": "alert('Hello World');",
+	"option4": "alertBox('Hello World');",
+	"answer": ""
+}, {
+	"question": "Which is a single integrated circuit?",
+	"option1": "Gate",
+	"option2": "Mother Board",
+	"option3": "Chip",
+	"option4": "CPU",
+	"answer": "1"
+}, {
+	"question": "C is ?",
+	"option1": "A third generation high level language",
+	"option2": "A machine language",
+	"option3": "An assembly language",
+	"option4": "All of the above",
+	"answer": "1"
+}, {
+	"question": "Web pages are written using ?",
+	"option1": "FTP",
+	"option2": "HTTP",
+	"option3": "HTML",
+	"option4": "URL",
+	"answer": "3"
+}, {
+	"question": "UNIVAC is an example of",
+	"option1": "First generation computer",
+	"option2": "Second generation computer",
+	"option3": "Third generation computer",
+	"option4": "Fourth generation computer",
+	"answer": "1"
+}, {
+	"question": "Which of the following is an example of non volatile memory ?",
+	"option1": "VLSI",
+	"option2": "LSI",
+	"option3": "ROM",
+	"option4": "RAM",
+	"answer": "3"
+}, {
+	"question": "Graphic interfaces were first used in a xerox product is called ?",
+	"option1": "Ethernet",
+	"option2": "Inter LISP",
+	"option3": "Small talk",
+	"option4": "Zeta LISP",
+	"answer": "1"
+}, {
+	"question": "Find the odd one out ?",
+	"option1": "ORACLE",
+	"option2": "SYBASE",
+	"option3": "C",
+	"option4": "INFORMIX",
+	"answer": "3"
+}, {
+	"question": "The ------ is the administrative section of the computer system?",
+	"option1": "Memory Unit",
+	"option2": "Input Unit",
+	"option3": "Central Processing Unit",
+	"option4": "Control Unit",
+	"answer": "3"
+}];
